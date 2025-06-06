211573e29f1f7d98bace92be98d03d5fda2d6907
wbin01 <wildenalisson@outlook.com>
2025-06-06 19:51:31 -0300

    mvc prototype

--- cell/data/mvc_prototype.py
+++ cell/data/mvc_prototype.py
@@ -1,33 +1,66 @@
 #/usr/bin/env python3
-class Ui(object):
-	# tab = 0
-	def __init__(self):
-		self.qml_code = (
-			'\n'
-			'\nMainFrame {'
-			'\n    id: mainFrame')
+class Layout(object):
+	pass
+
+class Widget(object):
+	pass
+
+class Layout(Layout):
+	def __init__(self, object_id: str, *args, **kwargs) -> None:
+		self.object_id = object_id
+		self.qml_code = None
 		self.added_objects = []
 
-	def add(self, obj):
+	def add(self, obj) -> Layout | Widget:
 		self.added_objects.append(obj)
+		return obj
 
 
-class Button(Ui):
-	def __init__(self, id):
-		super().__init__()
+class Widget(Widget):
+	def __init__(self, object_id: str, *args, **kwargs) -> None:
+		self.object_id = object_id
+		self.qml_code = None
+
+
+class MainFrame(Layout):
+	def __init__(self, *args, **kwargs) -> None:
+		super().__init__('mainFrame', *args, **kwargs)
+		self.object_id = 'mainFrame'
+		self.qml_code = (
+			'MainFrame {'
+			f'\n    id: mainFrame')
+		self.added_objects = []
+
+
+class Box(Layout):
+	def __init__(self, object_id: str, *args, **kwargs) -> None:
+		super().__init__(object_id, *args, **kwargs)
+		self.object_id = object_id
+		self.qml_code = (
+			'\n'
+			'\nBox {'
+			f'\n    id: {self.object_id}')
+		self.added_objects = []
+
+
+class Button(Widget):
+	def __init__(self, object_id: str, *args, **kwargs) -> None:
+		super().__init__(object_id, *args, **kwargs)
+		self.object_id = object_id
 		self.qml_code = (
 			'\n'
 			'\nButton {'
-			f'\n    id: {id}')
+			f'\n    id: {self.object_id}')
 
 
-class Label(Ui):
-	def __init__(self, id):
-		super().__init__()
+class Label(Widget):
+	def __init__(self, object_id: str, *args, **kwargs) -> None:
+		super().__init__(object_id, *args, **kwargs)
+		self.object_id = object_id
 		self.qml_code = (
 			'\n'
 			'\nLabel {'
-			f'\n    id: {id}')
+			f'\n    id: {self.object_id}')
 
 
 class Model(object):
@@ -35,57 +68,54 @@ class Model(object):
 	pass
 
 
-class Controller(object):
-	def __init__(self):
+class Handler(object):
+	def __init__(self) -> None:
 		self.ui = ''
-		# Mais codigo de controle abaixo
+		# ...
 
 	def build_ui(self, ui):
+		
 		for obj in ui.added_objects:
-			self.build_ui(obj)
-			ui.qml_code += '\n'.join(
-				['    '  + x if x else ''
-				for x in obj.qml_code.split('\n')])
-		ui.qml_code += '\n}'
+			if isinstance(obj, Layout):
+				self.build_ui(obj)
 
-		self.ui = ui.qml_code
+			ui.qml_code += '\n'.join(
+				['    ' + x if x else ''
+				for x in obj.qml_code.split('\n')]) + '\n    }'
+
+		self.ui = ui.qml_code + '\n}'
 
 
 class Application(object):
-	def __init__(self, controller):
+	def __init__(self, controller) -> None:
 		self.controller = controller
-		# Mais codigo de integração abaixo
+		# ...
 
 	def exec(self):
 		print(self.controller.ui)
 
 
 if __name__ == '__main__':
-	class MainView(Ui):
-		def __init__(self):
-			super().__init__()
+	class View(MainFrame):
+		def __init__(self, *args, **kwargs) -> None:
+			super().__init__(*args, **kwargs)
 			
-			button = Button('button')
-			self.add(button)
+			button = self.add(Button('button'))
+			label = self.add(Label('label'))
 
-			label = Label('label')
-			self.add(label)
+			box1 = self.add(Box('box1'))
+			button1 = box1.add(Button('button1'))
+			label1 = box1.add(Label('label1'))
 
-			button2 = Button('button2')
-			label.add(button2)
-
-			label2 = Label('label2')
-			label.add(label2)
-
-			label3 = Label('label3')
-			label2.add(label3)
+			box2 = box1.add(Box('box2'))
+			label2 = box2.add(Label('label2'))
 
 
-	class Controller(Controller):
-		def __init__(self):
+	class Controller(Handler):
+		def __init__(self) -> None:
 			super().__init__()
-			self.build_ui(MainView())
-			# Mais códigos de controle daqui para baixo
+			self.build_ui(View())
+			# ...
 
 
 	app = Application(Controller())
