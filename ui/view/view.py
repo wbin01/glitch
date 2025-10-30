13c322700fe9b9ef969ab48ff6a245d405cf8aea
wbin01 <wildenalisson@outlook.com>
2025-10-30 15:09:07 -0300

    visible View property

--- ui/view/view.py
+++ ui/view/view.py
@@ -10,3 +10,13 @@ class View(UI):
 
     def __repr__(self) -> str:
         return self.__class__.__name__
+
+    @property
+    def visible(self) -> bool:
+        """..."""
+        return self._QtObject__property('visible')
+
+    @visible.setter
+    def visible(self, visible: bool) -> None:
+        visible = 'true' if visible else 'false'
+        self._QtObject__set_property('visible', visible)
