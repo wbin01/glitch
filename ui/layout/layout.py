88d256284f8bc72cc035b862114f8b9056d0d3ab
wbin01 <wildenalisson@outlook.com>
2025-11-07 18:29:30 -0300

    align binding

--- ui/layout/layout.py
+++ ui/layout/layout.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python3
+from PySide6.QtQml import QQmlComponent, QQmlEngine, QQmlContext
+from PySide6.QtCore import QUrl, Qt
+
 from ..ui import UI
 from ..mixin import Add
 from ...enum.align import Align
 
+
+binding = """
+    Binding {
+        target: <id>.Layout
+        property: "alignment"
+        value: <id>.layoutAlignment
+    }
+    Binding {
+        target: <id>.Layout
+        property: "fillWidth"
+        value: <id>.layoutFillWidth
+    }
+    Binding {
+        target: <id>.Layout
+        property: "fillHeight"
+        value: <id>.layoutFillHeight
+    }
+    // +
+"""
 
 class Layout(Add, UI):
     """..."""
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, base='Layout', **kwargs)
         self._QtObject__set_property('clip', 'true')
+        self.__align_has_set = False
 
         self.__aligns = {
             Align.BASE_LINE: 'Qt.AlignBaseline',
@@ -26,42 +49,47 @@ class Layout(Add, UI):
             Align.V_CENTER: 'Qt.AlignVCenter',
             Align.NONE: '0'}
 
-        self._QtObject__set_property('Layout.alignment', 'alignment')
-        self._QtObject__set_property('property int alignment', 'Qt.AlignTop')
-        
-        self._QtObject__set_property('Layout.fillWidth', 'fillWidth')
-        self._QtObject__set_property('property bool fillWidth', 'true')
-        
-        self._QtObject__set_property('Layout.fillHeight', 'fillHeight')
-        self._QtObject__set_property('property bool fillHeight', 'false')
-
     def __repr__(self) -> str:
         return self.__class__.__name__
 
     @property
     def align(self) -> tuple:
         """..."""
-        if not self._QtObject__obj:
-            return Align.TOP, True, False
+        if not self._QtObject__obj: return Align.TOP, True, True
 
         fill_width = self._QtObject__property('fillWidth')
         fill_height = self._QtObject__property('fillHeight')
         alignment = self._QtObject__property('alignment')
 
-        for align in self.__aligns.keys():
-            if align.value == alignment:
-                return align, fill_width, fill_height
+        if not fill_width: fill_width = True
+        if not fill_height: fill_height = True
+        if not alignment: alignment = 'Qt.AlignTop'
+
+        for key, value in self.__aligns.items():
+            if value == alignment: return key, fill_width, fill_height
 
     @align.setter
     def align(self, align: Align | tuple) -> None:
+        if not align: return
+
+        if not self.__align_has_set:
+            self._QtObject__set_property('property bool fillWidth', 'true')
+            self._QtObject__set_property('property bool fillHeight', 'true')
+            self._QtObject__set_property(
+                'property int alignment', 'Qt.AlignTop')
+            
+            self._QtObject__set_property('Layout.alignment', 'alignment')
+            self._QtObject__set_property('Layout.fillWidth', 'fillWidth')
+            self._QtObject__set_property('Layout.fillHeight', 'fillHeight')
+
+            self._QtObject__qml = self._QtObject__qml.replace('// +', binding)
+            self.__align_has_set = True
+
         if isinstance(align, Align):
             if not self._QtObject__obj:
                 self._QtObject__set_property('alignment', self.__aligns[align])
             else:
                 self._QtObject__set_property('alignment', align.value)
-            return
-
-        if not align:
             return
 
         fill_width = None
@@ -76,13 +104,43 @@ class Layout(Add, UI):
             align, fill_width, fill_height = align[:3]
         
         if fill_width is not None:
-            self._QtObject__set_property('fillWidth', fill_width)
+            if not self._QtObject__obj:
+                self._QtObject__set_property('fillWidth', fill_width)
+            else:
+                self.__binding(self._QtObject__obj, 'fillWidth', fill_width)
 
         if fill_height is not None:
-            self._QtObject__set_property('fillHeight', fill_height)
+            if not self._QtObject__obj:
+                self._QtObject__set_property('fillHeight', fill_height)
+            else:
+                self.__binding(self._QtObject__obj, 'fillHeight', fill_height)
 
         if align is not None:
             if not self._QtObject__obj:
                 self._QtObject__set_property('alignment', self.__aligns[align])
             else:
-                self._QtObject__set_property('alignment', align.value)
+                self.__binding(
+                    self._QtObject__obj, 'alignment', int(align.value))
+
+    def __binding(self, item, layout_prop, value):
+        engine = QQmlEngine.contextForObject(item).engine()
+        qml_code = f"""
+            import QtQuick
+            import QtQuick.Layouts
+
+            Binding {{
+                target: layoutTarget.Layout
+                property: "{layout_prop}"
+                value: bindingValue
+            }}
+        """
+
+        component = QQmlComponent(engine)
+        component.setData(qml_code.encode('utf-8'), QUrl())
+
+        context = QQmlContext(engine.rootContext())
+        context.setContextProperty('layoutTarget', item)
+        context.setContextProperty('bindingValue', value)
+
+        binding_ = component.create(context)
+        binding_.setParent(item)
