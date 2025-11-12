e6d83bea8978c495bc5f8b34a736f6f68daba74b
wbin01 <wildenalisson@outlook.com>
2025-11-12 01:34:26 -0300

    rm enums

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from .core import Application, Cursor, Signal
-from .enum import Align, Hint, Shape, Size
+from .enum import Align, Hint, Shape
 # from .platform_ import Icons, OSDesk, Style
 from .ui import UI
 from .ui.frame import AppFrame
--- enum/__init__.py
+++ enum/__init__.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 from .align import Align
-# from .event import Event
 from .hint import Hint
 from .shape import Shape
-# from .orientation import Orientation
-from .size import Size
