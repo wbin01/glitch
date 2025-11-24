2c68bf321dea859b34325035eaf75bcd98c1ba9b
wbin01 <wildenalisson@outlook.com>
2025-11-24 19:01:47 -0300

    Panel V anim

--- ui/frame/panel.py
+++ ui/frame/panel.py
@@ -17,11 +17,12 @@ class Panel(Frame):
 
         self._app_signal.connect(self.__app_shape)
         self.__anim = None
-        self.__y = 0
-        self.__x = 0
+        self.__fy = 0
+        self.__fx = 0
         self.__width = 300
-        self.__float_width = False
-        self.__float_height = True
+        self.__float = True
+        self.__side = 0
+        self.__size = 300
 
     def __repr__(self) -> str:
         return self.__class__.__name__
@@ -29,52 +30,80 @@ class Panel(Frame):
     @property
     def x(self) -> int:
         """..."""
-        return self.__x
+        return self.__fx
 
     @x.setter
     def x(self, x: int) -> None:
-        self.__x = x
+        self.__fx = x
 
     @property
     def y(self) -> int:
         """..."""
-        return self.__y
+        return self.__fy
 
     @y.setter
     def y(self, y: int) -> None:
-        self.__y = y
+        self.__fy = y
 
-    def open(self, side=1) -> None:
+    def open(self, side=2) -> None:
         """..."""
-        height = (self.__y * 2 if self.__float_height else self.__y)
-        # width = (self.__x * 2 if self.__float_width else self.__x)
-        if self._QtObject__obj:
-            self._QtObject__obj.open()
-            if self._app.shape == Shape.MAX or self._app.shape == Shape.FULL:
-                self.height = int(self._app.height[0]) + 4 - height
-                self._QtObject__set_property('y', self.__y - 1)
-                end = self.__x - 1 if side == 0 else self._app.width[0] - 301 - self.__x
-            else:
-                self.height = int(self._app.height[0]) - 2 - height
-                self._QtObject__set_property('y', self.__y)
-                end = self.__x if side == 0 else self._app.width[0] - 302 - self.__x
+        self.__side = side
+        fy = (self.__fy * 2 if self.__float else self.__fy)
+        fx = (self.__fx * 2 if self.__float else self.__fx)
+        height = int(self._app.height[0])
+        width = int(self._app.width[0])
 
-            start = -300 if side == 0 else self._app.width[0] + 300
-            self.__anim = None
-            slide_in = QtCore.QPropertyAnimation(self._QtObject__obj, b"x")
-            slide_in.setDuration(300)
-            slide_in.setStartValue(start)
-            slide_in.setEndValue(end)
-            slide_in.setEasingCurve(QtCore.QEasingCurve.OutCubic)
-            fade_in = QtCore.QPropertyAnimation(self._QtObject__obj, b"opacity")
-            fade_in.setDuration(300)
-            fade_in.setStartValue(0)
-            fade_in.setEndValue(1)
-            fade_in.setEasingCurve(QtCore.QEasingCurve.OutCubic)
-            self.__anim = QtCore.QParallelAnimationGroup()
-            self.__anim.addAnimation(slide_in)
-            self.__anim.addAnimation(fade_in)
-            self.__anim.start()
+        if self._QtObject__obj and self.__side == 0 or self.__side == 1:
+            anim_type = b'x'
+            self.width = self.__size
+            if self._app.shape == Shape.MAX or self._app.shape == Shape.FULL:
+                self.height = height + 4 - fy
+                self._QtObject__set_property('y', self.__fy - 1)
+                size = (self.__size + 1) + self.__fx
+                end = self.__fx - 1 if self.__side == 0 else width - size
+            else:
+                self.height = height - 2 - fy
+                self._QtObject__set_property('y', self.__fy)
+                size = (self.__size + 2) + self.__fx
+                end = self.__fx if self.__side == 0 else width - size
+
+            start = -self.__size if self.__side == 0 else width + self.__size
+
+        if self._QtObject__obj and self.__side == 2 or self.__side == 3:
+            anim_type = b'y'
+            self.height = self.__size
+            if self._app.shape == Shape.MAX or self._app.shape == Shape.FULL:
+                self.width = width - fx
+                self._QtObject__set_property('x', self.__fx - 1)
+
+                size = (self.__size + 1) + self.__fy
+                end = self.__fy - 1 if self.__side == 2 else height - size
+            else:
+                self.width = width - 2 - fx
+                self._QtObject__set_property('x', self.__fx)
+                size = (self.__size + 2) + self.__fy
+                end = self.__fy if self.__side == 2 else height - size
+
+
+            start = -self.__size if self.__side == 2 else height
+
+        self._QtObject__obj.open()
+
+        self.__anim = None
+        slide_in = QtCore.QPropertyAnimation(self._QtObject__obj, anim_type)
+        slide_in.setDuration(300)
+        slide_in.setStartValue(start)
+        slide_in.setEndValue(end)
+        slide_in.setEasingCurve(QtCore.QEasingCurve.OutCubic)
+        fade_in = QtCore.QPropertyAnimation(self._QtObject__obj, b'opacity')
+        fade_in.setDuration(300)
+        fade_in.setStartValue(0)
+        fade_in.setEndValue(1)
+        fade_in.setEasingCurve(QtCore.QEasingCurve.OutCubic)
+        self.__anim = QtCore.QParallelAnimationGroup()
+        self.__anim.addAnimation(slide_in)
+        self.__anim.addAnimation(fade_in)
+        self.__anim.start()
 
     def __app_shape(self) -> None:
         self._app._shape_signal.connect(lambda: self._QtObject__obj.close())
