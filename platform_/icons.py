8d51d1414fbc03e08e337f530db0f46107637d95
wbin01 <wildenalisson@outlook.com>
2026-01-20 18:33:40 -0300

    icon theme sep

--- platform_/icons.py
+++ platform_/icons.py
@@ -82,23 +82,55 @@ class Icons(object):
             return self.__icon_theme
 
     def icon_source(
-            self, icon: str | None, size: int = 16, dark: bool = None
-            ) -> str:
+            self, icon: str | None, size: int = 16, dark: bool = None) -> str:
         """..."""
         if not self.__icon_theme:
             self.icon_theme()
 
         if self.__desktop_environment == 'plasma':
-            if dark or dark is None and 'dark' in self.__icon_theme.lower():
-                self.__icon_path = self.__path/'static'/'icons'/'linux-qt-dark'
-            else:
-                self.__icon_path = self.__path/'static'/'icons'/'linux-qt'
-        else:
-            if dark or dark is None and 'dark' in self.__icon_theme.lower():
-                self.__icon_path =self.__path/'static'/'icons'/'linux-gtk-dark'
-            else:
-                self.__icon_path = self.__path/'static'/'icons'/'linux-gtk'
+            return self.__icon_source_plasma(icon, size, dark)
+        elif self.__desktop_environment == 'cinnamon':
+            return self.__icon_source_cinnamon(icon, size, dark)
 
+    def icon_theme_variant(
+            self, theme: str = None, dark: bool = True) -> str | None:
+        """Dark or light variant icon theme"""
+        if not theme:
+            theme = self.icon_theme()
+
+        new_theme = ''
+        for path in self.__icon_theme_paths:
+            for suffix in self.__dark_suffixes:
+                
+                if dark and 'dark' not in theme.lower():
+                    if 'light' in theme.lower():
+                        
+                        for x in self.__light_suffixes:
+                            theme = theme.replace(x, '')
+
+                    if Path(path + theme + suffix).exists():
+                        new_theme = theme + suffix
+
+                elif not dark and 'dark' in theme.lower():
+                    temp = theme.replace(suffix, '')
+                    if Path(path+temp).exists() and 'dark' not in temp.lower():
+                        new_theme = temp
+
+        return new_theme if new_theme else None
+
+    def icon_theme_dark_variant(self, theme: str = None) -> str | None:
+        return self.icon_theme_variant(theme, True)
+
+    def icon_theme_light_variant(self, theme: str = None) -> str | None:
+        return self.icon_theme_variant(theme, False)
+
+    def __icon_source_plasma(
+            self, icon: str | None, size: int = 16, dark: bool = None) -> str:
+        if dark or dark is None and 'dark' in self.__icon_theme.lower():
+            self.__icon_path = self.__path/'static'/'icons'/'linux-qt-dark'
+        else:
+            self.__icon_path = self.__path/'static'/'icons'/'linux-qt'
+        
         if not icon:
             return str(self.__path / 'static' / 'icons' / 'empty.svg')
 
@@ -156,43 +188,44 @@ class Icons(object):
         if icon_path:
             return icon_path
         
-        if self.__desktop_environment != 'plasma':
-            icon = icon + '-symbolic.svg'
-        else:
-            icon = icon + '.svg'
-
+        icon = icon + '.svg'
         path = self.__icon_path / icon
         return str(path) if path.exists() else str(
             self.__path / 'static' / 'icons' / 'empty.svg')
 
-    def icon_theme_variant(
-            self, theme: str = None, dark: bool = True) -> str | None:
-        """Dark or light variant icon theme"""
-        if not theme:
-            theme = self.icon_theme()
+    def __icon_source_cinnamon(
+            self, icon: str | None, size: int = 16, dark: bool = None) -> str:
+        if dark or dark is None and 'dark' in self.__icon_theme.lower():
+            self.__icon_path =self.__path/'static'/'icons'/'linux-gtk-dark'
+        else:
+            self.__icon_path = self.__path/'static'/'icons'/'linux-gtk'
 
-        new_theme = ''
-        for path in self.__icon_theme_paths:
-            for suffix in self.__dark_suffixes:
-                
-                if dark and 'dark' not in theme.lower():
-                    if 'light' in theme.lower():
-                        
-                        for x in self.__light_suffixes:
-                            theme = theme.replace(x, '')
+        if not icon:
+            return str(self.__path / 'static' / 'icons' / 'empty.svg')
 
-                    if Path(path + theme + suffix).exists():
-                        new_theme = theme + suffix
+        if '/' in icon:
+            if not Path(icon).exists():
+                return str(self.__path / 'static' / 'icons' / 'empty.svg')
+            return icon
 
-                elif not dark and 'dark' in theme.lower():
-                    temp = theme.replace(suffix, '')
-                    if Path(path+temp).exists() and 'dark' not in temp.lower():
-                        new_theme = temp
+        variant_theme = self.__icon_theme
+        if dark and 'dark' not in self.__icon_theme.lower():
+            variant_theme = self.icon_theme_dark_variant(self.__icon_theme)
+        elif not dark and 'dark' in self.__icon_theme.lower():
+            variant_theme = self.icon_theme_light_variant(self.__icon_theme)
 
-        return new_theme if new_theme else None
+        if variant_theme: self.__icon_theme = variant_theme
+        
+        icon_path = IconTheme.getIconPath(
+            iconname=icon,
+            size=size,
+            theme=self.__icon_theme,
+            extensions=['png', 'svg', 'xpm'])
 
-    def icon_theme_dark_variant(self, theme: str = None) -> str | None:
-        return self.icon_theme_variant(theme, True)
-
-    def icon_theme_light_variant(self, theme: str = None) -> str | None:
-        return self.icon_theme_variant(theme, False)
+        if icon_path:
+            return icon_path
+        
+        icon = icon + '.svg'
+        path = self.__icon_path / icon
+        return str(path) if path.exists() else str(
+            self.__path / 'static' / 'icons' / 'empty.svg')
